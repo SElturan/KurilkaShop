da2737b46fdae6effb7eefa5af3b6752db3e2571
SElturan <138645249+SElturan@users.noreply.github.com>
2024-01-05 17:57:28 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-http_api = 'http://31.129.97.107:8000'
-CHANNEL_ID = '@kurilka_shop_spb'
-TOKEN = '5864205073:AAHLMIsgWaOdkKcMqxWbFJiYFqcnjJJf8Zs'
+http_api = 'API'
+CHANNEL_ID = '@username'
+TOKEN = 'TOKEN'
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
